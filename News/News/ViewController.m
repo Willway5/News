47562e4e496b914d20727f8cebc5073847028667
Will's GitHub <2451105570@qq.com>
2016-05-19 20:34:33 +0800

    origin

---
+++ News/News/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  News
+//
+//  Created by Will on 16/5/19.
+//  Copyright © 2016年 MT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
